c7c3afdf417c1bd8afe9bf37185d91672289cdf8
andreagrandi <andrea.grandi@tiscali.it>
2025-11-17 16:01:37 +0000

    minors

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -59,8 +59,8 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        joystick,
-        twist_mux,
+        #joystick,
+        #twist_mux,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
